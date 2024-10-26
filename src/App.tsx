80b516d123f63760b5c5f7df91fb9849fd788ade
itsmefminsaf <itsmefminsaf@gmail.com>
2024-10-26 17:30:38 +0530

    Project created

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <h1 className="w-fit bg-black px-4 py-2 text-4xl font-extrabold text-white">
+        Prest
+      </h1>
+    </main>
+  );
+};
+
+export default App;
