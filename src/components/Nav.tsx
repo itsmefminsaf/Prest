37fd8bca503ad30d3f2a4fdd7b96fd44217cc538
itsmefminsaf <itsmefminsaf@gmail.com>
2024-11-07 17:06:26 +0530

    logo included

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,12 @@
+import logo from "../assets/logo.svg";
+
 const Nav = () => {
   return (
-    <nav className="fixed left-[5%] top-3 z-50 flex w-[90%] justify-between rounded-full border-8 border-black bg-aliceblue p-4">
-      <div className="">Prest</div>
-      <ul className="flex flex-col justify-around gap-2 md:flex-row">
+    <nav className="fixed left-[5%] top-3 z-50 flex w-[90%] justify-between rounded-full border-8 border-black bg-aliceblue p-2">
+      <div className="relative h-14 w-36">
+        <img src={logo} alt="logo" className="absolute h-full w-full" />
+      </div>
+      <ul className="m-2 flex flex-col justify-around gap-2 md:flex-row">
         <li className="li-item">
           <a href="#home">Home</a>
         </li>
@@ -19,6 +23,9 @@ const Nav = () => {
           <a href="#contact">Contact Us</a>
         </li>
       </ul>
+      <button className="rounded-full border-cyan-500 bg-black px-5 py-1 text-xl font-extrabold text-white hover:rounded-l-none hover:border-l-4 hover:pr-8">
+        Book now!
+      </button>
     </nav>
   );
 };
