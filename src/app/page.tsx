5a73cae33d86871c6527c78bf7bbabafd214e740
itsmefminsaf <itsmefminsaf@gmail.com>
2025-03-12 15:55:46 +0530

    feat:responsiveness for hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,14 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <section className="relative flex h-screen flex-col items-center gap-5 px-2 pt-20 pb-5 sm:h-auto sm:flex-row-reverse sm:justify-between">
-        <img src="/assets/front.jpg" className="rounded-4xl p-2 sm:w-1/2" />
-        <h1 className="text-fg-secondary text-center text-3xl font-extrabold sm:w-1/2">
+      <section className="relative flex h-screen flex-col items-center gap-5 px-2 pt-20 pb-5 sm:flex-col-reverse sm:justify-end sm:pt-32 md:h-auto md:flex-row-reverse md:justify-between md:pt-20">
+        <div className="grid sm:grid-cols-2 sm:grid-rows-2 md:w-1/2 md:grid-cols-[3fr_1fr] md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-[3fr_1fr] xl:grid-cols-2 xl:grid-rows-2">
+          <img src="/assets/front.jpg" className="front-img-large" />
+          <img src="/assets/front2.jpg" className="front-img" />
+          <img src="/assets/front3.jpg" className="front-img" />
+          <img src="/assets/front4.jpg" className="front-img" />
+        </div>
+        <h1 className="text-fg-secondary text-center text-3xl font-extrabold md:w-1/2">
           Welcome to the <br />
           <span className="text-fg text-4xl">Great 5 star Hotel</span>
         </h1>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header className="bg-fg-secondary/50 outline-fg/50 absolute top-4 left-1/2 z-50 grid w-[90%] max-w-[60rem] -translate-x-1/2 grid-cols-2 flex-wrap rounded-3xl p-3 px-5 outline-4 backdrop-blur-3xl sm:grid-cols-[auto_auto]">
       <img src="/assets/logo.svg" alt="Prest" className="h-7" />
       <button
-        className="bg-fg aspect-square place-self-end rounded-2xl p-2 text-white sm:hidden"
+        className="bg-fg aspect-square place-self-end rounded-2xl p-2 text-white md:hidden"
         onClick={() => {
           setIsNavOpen(!isNavOpen);
         }}
@@ -17,9 +17,9 @@ const Header = () => {
         {isNavOpen ? <ChevronUp /> : <ChevronDown />}
       </button>
       <nav
-        className={`sm:block ${isNavOpen ? "block" : "hidden"} animate-expand col-span-2 origin-top place-self-center sm:col-span-1 sm:place-self-end`}
+        className={`md:block ${isNavOpen ? "block" : "hidden"} animate-expand col-span-2 origin-top place-self-center md:col-span-1 md:place-self-end`}
       >
-        <ul className="flex h-full flex-col items-center justify-center text-xl text-white sm:flex-row sm:gap-3 sm:text-sm">
+        <ul className="flex h-full flex-col items-center justify-center text-xl text-white md:flex-row md:gap-3 md:text-sm">
           <li className="hover:bg-bg/50 hover:text-fg-secondary rounded-xl px-4 py-1 font-bold duration-200 hover:backdrop-blur-2xl">
             About
           </li>
